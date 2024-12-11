8f2cf3eecc61ec7157b90e7f225f09701db25f1b
lambda-dom <grodrigues.math@gmail.com>
2024-12-11 14:57:43 +0000

    Refactor parseTable; clean module
    Signed-off-by: lambda-dom <grodrigues.math@gmail.com>

--- src/Trisagion/Examples/Table.hs
+++ src/Trisagion/Examples/Table.hs
@@ -6,7 +6,6 @@ Parser for the .tbl tabular format.
 
 module Trisagion.Examples.Table (
     -- * Error types.
-    MismatchError (..),
     TableError (..),
 
     -- * Types.
@@ -17,35 +16,23 @@ module Trisagion.Examples.Table (
 
     -- * Parsers.
     parseTable,
-
-    -- ** Auxiliary parsers.
-    parseRows,
 ) where
 
 -- Imports.
 -- Base.
 import Data.Bifunctor (Bifunctor (..))
-import Data.List.NonEmpty (NonEmpty, nonEmpty)
-import Data.Void (absurd)
+import Data.List.NonEmpty (NonEmpty)
 
 -- Libraries.
-import Control.Monad.Except (MonadError(..))
-import Control.Monad.State (MonadState(..))
 import Data.Text (Text)
 import Data.Vector.Strict (Vector, fromList)
 
 -- Package.
-import Trisagion.Lib.NonEmpty (zipExact)
-import Trisagion.Types.ParseError (ParseError, makeParseError)
+import Trisagion.Types.ParseError (ParseError)
 import Trisagion.Get (Get)
 import Trisagion.Getters.Combinators (onParseError)
-import Trisagion.Getters.Combinators qualified as Getters (maybe)
-import Trisagion.Examples.Table.Parsers (Lines, parseHeader, parseFields, parseLine)
+import Trisagion.Examples.Table.Parsers (Lines, parseHeader, parseFields, parseRows)
 
-
-{- | The @MismatchError@ error type, raised when the number of values and field names are not equal. -}
-data MismatchError = MismatchError
-    deriving stock (Eq, Show)
 
 {- | The @TableError@ error type. -}
 data TableError
@@ -65,24 +52,6 @@ makeTable :: [a] -> Table a
 makeTable = Table . fromList
 
 
-{- | Parser for a non-empty list of .tbl table rows. -}
-parseRows :: NonEmpty Text -> Get Lines (ParseError Lines TableError) [NonEmpty (Text, Text)]
-parseRows fields = go
-    where
-        go = do
-            s <- get
-            r <- first absurd $ Getters.maybe parseLine
-            case r of
-                Nothing -> pure []
-                Just ts ->
-                    case nonEmpty ts of
-                        -- Case of empty line.
-                        Nothing -> go
-                        Just fs ->
-                            case zipExact fields fs of
-                                Nothing -> throwError $ makeParseError s RowError
-                                Just ps -> (ps :) <$> go
-
 {- | Parser for an entire .tbl table. -}
 parseTable
     :: (NonEmpty (Text, Text) -> a)     -- ^ Row conversion function.
@@ -90,4 +59,4 @@ parseTable
 parseTable f = do
         _      <- onParseError HeaderError parseHeader
         fields <- onParseError FieldsError parseFields
-        makeTable . fmap f <$> parseRows fields
+        bimap (fmap (const RowError)) (makeTable . fmap f) $ parseRows fields
