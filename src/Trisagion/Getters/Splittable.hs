7c69b3457f54f432572926ef064f5cef901cd7ea
lambda-dom <grodrigues.math@gmail.com>
2025-01-28 12:04:25 +0000

    Splittable parsers

--- src/Trisagion/Getters/Splittable.hs
+++ src/Trisagion/Getters/Splittable.hs
@@ -31,31 +31,36 @@ import Control.Monad.State (MonadState (..), gets)
 import Data.MonoTraversable (MonoFoldable (..), Element)
 
 -- Package.
-import Trisagion.Types.ParseError (ParseError, makeParseError)
+import Trisagion.Types.ParseError (makeParseErrorNoBacktrace)
+import Trisagion.Typeclasses.HasPosition (HasPosition)
 import Trisagion.Typeclasses.Splittable (Splittable (..))
-import Trisagion.Get (Get, eval)
+import Trisagion.Get (Get, skip, eval)
+import Trisagion.Getters.ParseError (Parser, validate)
 import Trisagion.Getters.Streamable (InputError (..), MatchError (..), ValidationError (..))
-import Trisagion.Getters.Combinators (skip, validate)
 
 
 {- | Run a parser isolated to a prefix of the stream.
 
-Any unconsumed input in the prefix is silently discarded.
+Any unconsumed input in the prefix is silently discarded. If such behavior is undesirable, guard the
+parser to run with an appropriate check.
 -}
 isolateWith
-    :: (s -> Maybe (s, s))      -- ^ Stream splitter. The @'Nothing'@ case signals insufficient input.
-    -> Get s e a                -- ^ Parser to run.
-    -> Get s (ParseError s (Either InputError e)) a
+    :: HasPosition s
+    => (s -> Maybe (s, s))          -- ^ Stream splitter. The @'Nothing'@ case signals insufficient input.
+    -> Get s e a                    -- ^ Parser to run.
+    -> Parser s (Either InputError e) a
 isolateWith h p = do
     xs <- get
     case h xs of
-        Nothing               -> throwError $ makeParseError xs (Left InsufficientInputError)
+        Nothing               -> throwError $ makeParseErrorNoBacktrace xs (Left InsufficientInputError)
         Just (prefix, suffix) ->
             case eval p prefix of
-                Left e -> throwError $ makeParseError prefix (Right e)
+                Left e -> throwError $ makeParseErrorNoBacktrace prefix (Right e)
                 Right x -> put suffix $> x
 
+
 {- | Get the rest of the input stream. -}
+{-# INLINE remainder #-}
 remainder :: Splittable s => Get s Void (PrefixOf s)
 remainder = do
     (prefix, suffix) <- gets getRemainder
@@ -65,12 +70,14 @@ remainder = do
 
 The parser does not error and it is guaranteed that the prefix has length equal or less than @n@.
 -}
+{-# INLINE takePrefix #-}
 takePrefix :: Splittable s => Word -> Get s Void (PrefixOf s)
 takePrefix n = do
     (prefix, suffix) <- gets $ getAt n
     put suffix $> prefix
 
 {- | Drop a fixed size prefix from the stream. -}
+{-# INLINE dropPrefix #-}
 dropPrefix :: Splittable s => Word -> Get s Void ()
 dropPrefix = skip . takePrefix
 
@@ -78,12 +85,13 @@ dropPrefix = skip . takePrefix
 
 note(s):
 
-    * The implementation requires computing the length of the prefix, which is @O(n)@ for some types
+    * Implementation requires computing the length of the prefix, which is @O(n)@ for some types
     (e.g. @Text@).
 -}
+{-# INLINE takeExact #-}
 takeExact
-    :: (Splittable s, MonoFoldable (PrefixOf s))
-    => Word -> Get s (ParseError s InputError) (PrefixOf s)
+    :: (HasPosition s, Splittable s, MonoFoldable (PrefixOf s))
+    => Word -> Parser s InputError (PrefixOf s)
 takeExact n = first (fmap (either absurd id)) $ validate v (first absurd $ takePrefix n)
     where
         v prefix =
@@ -98,9 +106,10 @@ note(s):
     * The implementation requires computing the length of the prefix, which is @O(n)@ for some types
     (e.g. @Text@).
 -}
+{-# INLINE match #-}
 match
-    :: (Splittable s, MonoFoldable (PrefixOf s), Eq (PrefixOf s))
-    => PrefixOf s -> Get s (ParseError s (Either InputError (MatchError (PrefixOf s)))) (PrefixOf s)
+    :: (HasPosition s, Splittable s, MonoFoldable (PrefixOf s), Eq (PrefixOf s))
+    => PrefixOf s -> Parser s (Either InputError (MatchError (PrefixOf s))) (PrefixOf s)
 match xs = validate v (takeExact (fromIntegral $ olength xs))
     where
         v prefix =
@@ -109,19 +118,22 @@ match xs = validate v (takeExact (fromIntegral $ olength xs))
                 else Left $ MatchError xs
 
 {- | Get the longest prefix from the streamable whose elements satisfy a predicate. -}
+{-# INLINE takeWith #-}
 takeWith :: Splittable s => (Element s -> Bool) -> Get s Void (PrefixOf s)
 takeWith p = do
     (prefix, suffix) <- gets $ getWith p
     put suffix $> prefix
 
 {- | Drop the longest prefix from the stream whose elements satisfy a predicate. -}
+{-# INLINE dropWith #-}
 dropWith :: Splittable s => (Element s -> Bool) -> Get s Void ()
 dropWith = skip . takeWith
 
 {- | Get the longest prefix with at least one element whose elements satisfy a predicate. -}
+{-# INLINE atLeastOneWith #-}
 atLeastOneWith
-    :: (Splittable s, MonoFoldable (PrefixOf s))
-    => (Element s -> Bool) -> Get s (ParseError s (Either InputError ValidationError)) (PrefixOf s)
+    :: (HasPosition s, Splittable s, MonoFoldable (PrefixOf s))
+    => (Element s -> Bool) -> Parser s (Either InputError ValidationError) (PrefixOf s)
 atLeastOneWith p = do
     s <- get
     xs <- first absurd $ takeWith p
@@ -131,5 +143,5 @@ atLeastOneWith p = do
             b <- gets onull
             -- Either not enough input or malformed one.
             if b
-                then throwError $ makeParseError s (Left InsufficientInputError)
-                else throwError $ makeParseError s (Right ValidationError)
+                then throwError $ makeParseErrorNoBacktrace s (Left InsufficientInputError)
+                else throwError $ makeParseErrorNoBacktrace s (Right ValidationError)
