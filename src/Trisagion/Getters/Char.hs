264a5b3230606684a633a7850bc61402ded55a8a
lambda-dom <grodrigues.math@gmail.com>
2025-01-28 14:00:37 +0000

    Char parsers

--- src/Trisagion/Getters/Char.hs
+++ src/Trisagion/Getters/Char.hs
@@ -1,7 +1,7 @@
 {- |
 Module: Trisagion.Getters.Char
 
-Parsers @('Streamable' s, 'Element' s ~ Char) => 'Get' s@.
+Parsers @('HasPosition' s, 'Element' s ~ Char) => 'Get' s@.
 -}
 
 module Trisagion.Getters.Char (
@@ -35,13 +35,13 @@ import Data.Void (Void, absurd)
 import Data.MonoTraversable (MonoFoldable (..), Element)
 
 -- Package.
-import Trisagion.Typeclasses.Streamable (Streamable)
+import Trisagion.Typeclasses.HasPosition (HasPosition)
 import Trisagion.Typeclasses.Splittable (Splittable (..))
 import Trisagion.Types.ParseError (ParseError)
 import Trisagion.Get (Get)
-import Trisagion.Getters.Combinators (validate)
-import qualified Trisagion.Getters.Combinators as Getters (maybe)
-import Trisagion.Getters.Streamable (InputError, MatchError, ValidationError (..), matchElem, satisfy, one) 
+import qualified Trisagion.Get as Getters (maybe)
+import Trisagion.Getters.ParseError (Parser, ValidationError (..), validate)
+import Trisagion.Getters.Streamable (InputError, MatchError, matchElem, satisfy, one) 
 import Trisagion.Getters.Splittable (takeWith, atLeastOneWith)
 
 
@@ -52,28 +52,28 @@ data Sign = Negative | Positive
 
 {- | Get a line feed (character @'\\n'@) from the stream. -}
 lf
-    :: (Streamable s, Element s ~ Char)
-    => Get s (ParseError s (Either InputError (MatchError Char))) Char
+    :: (HasPosition s, Element s ~ Char)
+    => Parser s (Either InputError (MatchError Char)) Char
 lf = matchElem '\n'
 
 {- | Get a carriage return (character @'\\r'@) from the stream. -}
 cr
-    :: (Streamable s, Element s ~ Char)
-    => Get s (ParseError s (Either InputError (MatchError Char))) Char
+    :: (HasPosition s, Element s ~ Char)
+    => Parser s (Either InputError (MatchError Char)) Char
 cr = matchElem '\r'
 
-{- | Parser for a possibly null prefix of whitespace. -}
+{- | Parser for a, possibly null, prefix of whitespace. -}
 spaces :: (Splittable s, Element s ~ Char) => Get s Void (PrefixOf s)
 spaces = takeWith isSpace
 
-{- | Parser for a possibly null prefix of non-whitespace characters. -}
+{- | Parser for a, possibly null, prefix of non-whitespace characters. -}
 notSpaces :: (Splittable s, Element s ~ Char) => Get s Void (PrefixOf s)
 notSpaces = takeWith (not . isSpace)
 
 {- | Parser for the number sign. -}
 sign
-    :: (Streamable s, Element s ~ Char)
-    => Get s (ParseError s (Either InputError ValidationError)) Sign
+    :: (HasPosition s, Element s ~ Char)
+    => Parser s (Either InputError ValidationError) Sign
 sign = validate v one
     where
         v x = case x of
@@ -83,14 +83,14 @@ sign = validate v one
 
 {- | Get a decimal digit from the streamable. -}
 digit
-    :: (Streamable s, Element s ~ Char)
-    => Get s (ParseError s (Either InputError ValidationError)) Char
+    :: (HasPosition s, Element s ~ Char)
+    => Parser s (Either InputError ValidationError) Char
 digit = satisfy isDigit
 
 {- | Parser for positive, integer numbers in decimal format. -}
 positive
-    :: (Splittable s, MonoFoldable (PrefixOf s), Element s ~ Char, Element (PrefixOf s) ~ Char)
-    => Get s (ParseError s (Either InputError ValidationError)) Word
+    :: (HasPosition s, Splittable s, MonoFoldable (PrefixOf s), Element s ~ Char, Element (PrefixOf s) ~ Char)
+    => Parser s (Either InputError ValidationError) Word
 positive = do
         digits <- atLeastOneWith isDigit
         let
@@ -104,12 +104,12 @@ positive = do
 
 {- | Transform a @'Word'@ parser into an @'Int'@-parser for signed numbers. -}
 signed
-    :: (Streamable s, Element s ~ Char)
+    :: (HasPosition s, Element s ~ Char)
     => Get s (ParseError s (Either InputError ValidationError)) Word
     -> Get s (ParseError s (Either InputError ValidationError)) Int
 signed p = do
     sgn <- first absurd (fromMaybe Positive <$> Getters.maybe sign)
     number <- fromIntegral <$> p
-    if sgn == Positive
-        then pure number
-        else pure (-number)
+    case sgn of
+        Positive -> pure number
+        Negative -> pure (-number)
