7bbde9b4091616b25539ab1dcafe59b2614808be
lambda-dom <grodrigues.math@gmail.com>
2025-03-16 14:34:27 +0000

    `Applicative` parsers.

--- src/Trisagion/Parsers/Combinators.hs
+++ src/Trisagion/Parsers/Combinators.hs
@@ -10,11 +10,23 @@ module Trisagion.Parsers.Combinators (
     observe,
     lookAhead,
     maybe,
+
+    -- * 'Functor' parsers.
+    skip,
+
+    -- * 'Applicative' parsers.
+    before,
+    after,
+    between,
+    zip,
+    zipWith,
+    repeat,
+    sequence,
 ) where
 
 -- Imports.
 -- Prelude.
-import Prelude hiding (either, maybe)
+import Prelude hiding (either, maybe, repeat, sequence, zip, zipWith)
 import Prelude as Base (either)
 
 -- Base.
@@ -54,4 +66,60 @@ The difference with @'Control.Applicative.optional'@ is the more precise type si
 -}
 maybe :: Parser s e a -> Parser s Void (Maybe a)
 maybe p = Base.either (const Nothing) Just <$> observe p
+
+
+{- | Run the parser but discard the result.
+
+note(s):
+
+    * Various combinators have more efficient @skip*@ versions that avoid constructing intermediate
+    values; use those whenever possible.
+-}
+skip :: Parser s e a -> Parser s e ()
+skip = ($> ())
+
+
+{- | The parser @'before' b p@ parses @b@ and @p@ in succession, returning the result of @p@. -}
+before
+    :: Parser s e b                     -- ^ Parser to run first.
+    -> Parser s e a                     -- ^ Parser to run.
+    -> Parser s e a
+before = (*>)
+
+{- | The parser @'after' a p@ parses @p@ and @a@ in succession, returning the result of @p@. -}
+after
+    :: Parser s e b                     -- ^ Parser to run after.
+    -> Parser s e a                     -- ^ Parser to run.
+    -> Parser s e a
+after = flip (<*)
+
+{- | The parser @'between' o c p@ parses @o@, @p@ and @c@ in succession, returning the result of @p@. -}
+between
+    :: Parser s e b                     -- ^ Opening parser.
+    -> Parser s e c                     -- ^ Closing parser.
+    -> Parser s e a                     -- ^ Parser to run in-between.
+    -> Parser s e a
+between open close = before open . after close
+
+{- | Sequence two parsers and zip the results in a pair. -}
+zip :: Parser s e a -> Parser s e b -> Parser s e (a, b)
+zip = zipWith (,)
+
+{- | Sequence two parsers and zip the results with a binary function. -}
+zipWith :: (a -> b -> c) -> Parser s e a -> Parser s e b -> Parser s e c
+zipWith f p q = f <$> p <*> q
+
+{- | Run the parser @n@ times and return the list of results.
+
+It is guaranteed that the list of results has exactly @n@ elements.
+-}
+repeat :: Word -> Parser s e a -> Parser s e [a]
+repeat n p = go n
+    where
+        go 0 = pure []
+        go m = (:) <$> p <*> go (pred m)
+
+{- | Sequence a traversable of parsers and return the traversable of results. -}
+sequence :: Traversable t => t (Parser s e a) -> Parser s e (t a)
+sequence = sequenceA
 
