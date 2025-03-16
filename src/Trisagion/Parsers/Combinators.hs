3333dd2c3de8fd8f9add8f792db9126d65cda829
lambda-dom <grodrigues.math@gmail.com>
2025-03-16 13:50:50 +0000

    `lookAhead` and `maybe` parsers.

--- src/Trisagion/Parsers/Combinators.hs
+++ src/Trisagion/Parsers/Combinators.hs
@@ -8,21 +8,28 @@ module Trisagion.Parsers.Combinators (
     -- * Parsers without errors.
     value,
     observe,
+    lookAhead,
+    maybe,
 ) where
 
 -- Imports.
+-- Prelude.
+import Prelude hiding (either, maybe)
+import Prelude as Base (either)
+
 -- Base.
+import Data.Bifunctor (Bifunctor (..))
 import Data.Functor (($>))
-import Data.Void (Void)
+import Data.Void (Void, absurd)
 
 -- Libraries.
 import Control.Monad.State (MonadState (..))
 
 -- Package.
-import Trisagion.Parser (Parser, catchErrorWith)
+import Trisagion.Parser (Parser, catchErrorWith, eval)
 
 
-{- | Embed a value in the t'Parser' monad.
+{- | Embed a value in the 'Parser' monad.
 
 The difference with 'pure' from 'Applicative' is the more precise signature.
 -}
@@ -36,3 +43,15 @@ observe p = do
     catchErrorWith
         (Right <$> p)
         (\ e -> put s $> Left e)
+
+{- | Run the parser and return the result, but do not consume any input. -}
+lookAhead :: Parser s e a -> Parser s Void (Either e a)
+lookAhead p = eval p <$> first absurd get
+
+{- | Run the parser and return the result as a @'Just'@. If it errors, backtrack and return @'Nothing'@.
+
+The difference with @'Control.Applicative.optional'@ is the more precise type signature.
+-}
+maybe :: Parser s e a -> Parser s Void (Maybe a)
+maybe p = Base.either (const Nothing) Just <$> observe p
+
