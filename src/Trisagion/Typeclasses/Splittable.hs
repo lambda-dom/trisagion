4484d449bb517c57a7a2cb5dee7bdf3a737beaee
lambda-dom <grodrigues.math@gmail.com>
2025-03-18 15:40:09 +0000

    Renaming get -> split.

--- src/Trisagion/Typeclasses/Splittable.hs
+++ src/Trisagion/Typeclasses/Splittable.hs
@@ -25,32 +25,33 @@ import Trisagion.Typeclasses.Streamable (Streamable (..))
 Mirroring the laws for the 'Streamable' typeclass, the first law is:
 
 __Naturality__: With @('MonoFunctor' ('PrefixOf' s), 'ElementOf' ('PrefixOf' s) ~ 'ElementOf' s)@,
-for every @n@ and every @p@, both '@getAt' n@ and @'getWith' p@ are mononatural.
+for every @n@ and every @p@, both @splitAt n@ and @splitWith p@ are mononatural.
 
 For the second law, assuming @MonoFoldable ('PrefixOf' s)@ besides the above @'MonoFunctor'@
-constraint, then at the level of lists 'getAt' is 'Data.List.splitAt' and 'getWith' 'Data.List.span':
+constraint, then at the level of lists @splitAt@ is 'Data.List.splitAt' and @splitWith@ is
+'Data.List.span':
 
 __List identities__:
 
-prop> bimap monotoList monotoList . getAt n = splitAt n . monotoList
-prop> bimap monotoList monotoList . getWith p = span p . monotoList
+prop> bimap monotoList monotoList . splitAt n = splitAt n . monotoList
+prop> bimap monotoList monotoList . splitWith p = span p . monotoList
 
-The third and final law is a compatibility condition between 'getOne' and 'getAt':
+The third and final law is a compatibility condition between 'splitOne' and @splitAt@:
 
 __Compatibility__:
 
-prop> maybe [] (bimap singleton monotoList) . getOne = bimap monotoList monotoList . getAt 1
+prop> maybe [] (bimap singleton monotoList) . splitOne = bimap monotoList monotoList . splitAt 1
  -}
 class Streamable s => Splittable s where
-    {-# MINIMAL getAt, getWith #-}
+    {-# MINIMAL splitAt, splitWith #-}
 
     {- | The type of prefixes of the streamable. -}
     type PrefixOf s :: Type
 
     {- | Split the stream at index @n@ into a pair @(prefix, suffix)@. -}
-    getAt :: Word -> s -> (PrefixOf s, s)
+    splitAt :: Word -> s -> (PrefixOf s, s)
 
     {- | Split the stream into a pair @(prefix, suffix)@ using a predicate @p@.
     
     @prefix@ is the longest prefix whose elements satisfy @p@ and @suffix@ is the remainder. -}
-    getWith :: (ElementOf s -> Bool) -> s -> (PrefixOf s, s)
+    splitWith :: (ElementOf s -> Bool) -> s -> (PrefixOf s, s)
