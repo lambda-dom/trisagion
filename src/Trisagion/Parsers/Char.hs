1be85c5a815fcc1385caaf9f5c28652e3d3e3076
lambda-dom <grodrigues.math@gmail.com>
2025-03-26 14:52:30 +0000

    `Char` parsers.

---
+++ src/Trisagion/Parsers/Char.hs
@@ -0,0 +1,129 @@
+{- |
+Module: Trisagion.Parsers.Char
+
+Parsers @('Streamable' s, 'ElementOf' s ~ Char) => 'Parser' s@.
+-}
+
+module Trisagion.Parsers.Char (
+    -- * Types.
+    Sign (..),
+
+    -- * Newline parsers.
+    lf,
+    cr,
+
+    -- * Whitespace parsers.
+    spaces,
+    notSpaces,
+
+    -- * Numeric parsers.
+    sign,
+    digit,
+    positive,
+    signed,
+) where
+
+-- Imports.
+-- Base.
+import Data.Bifunctor (Bifunctor(..))
+import Data.Char (isSpace, isDigit, ord)
+import Data.Foldable (foldl')
+import Data.Maybe (fromMaybe)
+import Data.Void (Void, absurd)
+
+-- Libraries.
+-- non-Hackage libraries.
+import Data.MonoFunctor (ElementOf)
+import Data.MonoFoldable (MonoFoldable (..))
+
+-- Package.
+import Trisagion.Types.ParseError (ParseError)
+import Trisagion.Typeclasses.Streamable (Streamable)
+import Trisagion.Typeclasses.Splittable (Splittable (..))
+import Trisagion.Parser (Parser)
+import qualified Trisagion.Parsers.Combinators as Parsers (maybe)
+import Trisagion.Parsers.ParseError (ValidationError (..), validate)
+import Trisagion.Parsers.Streamable (InputError, matchElem, one, satisfy)
+import Trisagion.Parsers.Splittable (takeWith, atLeastOneWith)
+
+
+{- | The sign of a number. -}
+data Sign = Negative | Positive
+    deriving stock (Eq, Ord, Bounded, Enum, Show)
+
+
+{- | Parse a line feed (character @'\\n'@). -}
+lf
+    :: (Streamable s, ElementOf s ~ Char)
+    => Parser s (ParseError s (Either InputError (ValidationError Char))) Char
+lf = matchElem '\n'
+
+{- | Parse a carriage return (character @'\\r'@). -}
+cr
+    :: (Streamable s, ElementOf s ~ Char)
+    => Parser s (ParseError s (Either InputError (ValidationError Char))) Char
+cr = matchElem '\r'
+
+
+{- | Parse a, possibly null, prefix of whitespace. -}
+spaces :: (Splittable s, ElementOf s ~ Char) => Parser s Void (PrefixOf s)
+spaces = takeWith isSpace
+
+{- | Parse a, possibly null, prefix of non-whitespace characters. -}
+notSpaces :: (Splittable s, ElementOf s ~ Char) => Parser s Void (PrefixOf s)
+notSpaces = takeWith (not . isSpace)
+
+
+{- | Parse a number sign. -}
+sign
+    :: (Streamable s, ElementOf s ~ Char)
+    => Parser s (ParseError s (Either InputError (ValidationError Char))) Sign
+sign = validate v one
+    where
+        v x = case x of
+            y | y == '-' -> Right Negative
+            y | y == '+' -> Right Positive
+            _            -> Left $ ValidationError x
+
+{- | Parse a decimal digit. -}
+digit
+    :: (Streamable s, ElementOf s ~ Char)
+    => Parser s (ParseError s (Either InputError (ValidationError Char))) Char
+digit = satisfy isDigit
+
+{- | Parse a 'Word' in decimal format.
+
+note(s):
+
+    * no overflow checks are made.
+-}
+positive
+    :: (Splittable s, MonoFoldable (PrefixOf s), ElementOf s ~ Char, ElementOf (PrefixOf s) ~ Char)
+    => Parser s (ParseError s (Either InputError (ValidationError (PrefixOf s)))) Word
+positive = do
+        digits <- atLeastOneWith isDigit
+        let
+             n = monolength digits
+             xs = zip [n - 1, n - 2 .. 0] (monotoList digits)
+        pure $ foldl' (+) 0 [ value d i | (i, d) <- xs]
+    where
+        value :: Char -> Word -> Word
+        -- Returns implementation-dependent garbage for non-decimal digits.
+        value c n = fromIntegral (ord c - ord '0') *  10 ^ n
+
+{- | Transform a @'Word'@ parser into an 'Int'-parser for signed numbers.
+
+note(s):
+
+    * no overflow checks are made.
+-}
+signed
+    :: (Streamable s, ElementOf s ~ Char)
+    => Parser s (ParseError s (Either InputError (ValidationError (PrefixOf s)))) Word
+    -> Parser s (ParseError s (Either InputError (ValidationError (PrefixOf s)))) Int
+signed p = do
+    sgn <- first absurd (fromMaybe Positive <$> Parsers.maybe sign)
+    number <- fromIntegral <$> p
+    case sgn of
+        Positive -> pure number
+        Negative -> pure (-number)
