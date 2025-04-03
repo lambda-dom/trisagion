d315d5855155ace1703f9fa968d4e577064818e1
lambda-dom <35112850+lambda-dom@users.noreply.github.com>
2025-04-03 14:42:10 +0100

    `Char` parsers and tests. (#16)

---
+++ src/Trisagion/Parsers/Char.hs
@@ -0,0 +1,140 @@
+{- |
+Module: Trisagion.Parsers.Char
+
+Parsers @('Streamable' s, 'ElementOf' s ~ Char) => 'Parser' s@.
+-}
+
+module Trisagion.Parsers.Char (
+    -- * Types.
+    Sign (..),
+
+    -- * Newline parsers.
+    lf,
+    cr,
+
+    -- * Whitespace parsers.
+    spaces,
+    notSpaces,
+
+    -- * Numeric parsers.
+    sign,
+    digit,
+    positive,
+    signed,
+
+    -- * Other lexemes.
+    comment,
+) where
+
+-- Imports.
+-- Base.
+import Data.Bifunctor (Bifunctor (..))
+import Data.Char (isSpace, isDigit, ord)
+import Data.Foldable (foldl')
+import Data.Maybe (fromMaybe)
+import Data.Void (Void, absurd)
+
+-- non-Hackage libraries.
+import Data.MonoFunctor (MonoFunctor (..))
+import Data.MonoFoldable (MonoFoldable (..))
+
+-- Package.
+import Trisagion.Typeclasses.HasPosition (HasPosition)
+import Trisagion.Typeclasses.Streamable (Streamable)
+import Trisagion.Typeclasses.Splittable (Splittable (..))
+import Trisagion.Parser (Parser, ParserPE, InputError, (:+:), takePrefixWith, one)
+import qualified Trisagion.Parsers.Combinators as Parsers (maybe)
+import Trisagion.Parsers.ParseError (ValidationError (..), validate)
+import Trisagion.Parsers.Streamable (matchElem, satisfy)
+import Trisagion.Parsers.Splittable (atLeastOneWith)
+
+
+{- | The sign of a number. -}
+data Sign = Negative | Positive
+    deriving stock (Eq, Ord, Bounded, Enum, Show)
+
+
+{- | Parse a line feed (character @'\\n'@). -}
+lf
+    :: (HasPosition s, Streamable s, ElementOf s ~ Char)
+    => ParserPE s (InputError :+: ValidationError Char) Char
+lf = matchElem '\n'
+
+{- | Parse a carriage return (character @'\\r'@). -}
+cr
+    :: (HasPosition s, Streamable s, ElementOf s ~ Char)
+    => ParserPE s (InputError :+: ValidationError Char) Char
+cr = matchElem '\r'
+
+
+{- | Parse a, possibly null, prefix of whitespace. -}
+spaces :: (Splittable s, ElementOf s ~ Char) => Parser s Void (PrefixOf s)
+spaces = takePrefixWith isSpace
+
+{- | Parse a, possibly null, prefix of non-whitespace characters. -}
+notSpaces :: (Splittable s, ElementOf s ~ Char) => Parser s Void (PrefixOf s)
+notSpaces = takePrefixWith (not . isSpace)
+
+
+{- | Parse a number sign. -}
+sign
+    :: (HasPosition s, Streamable s, ElementOf s ~ Char)
+    => ParserPE s (InputError :+: ValidationError Char) Sign
+sign = validate v one
+    where
+        v x = case x of
+            y | y == '-' -> Right Negative
+            y | y == '+' -> Right Positive
+            _            -> Left $ ValidationError x
+
+{- | Parse a decimal digit. -}
+digit
+    :: (HasPosition s, Streamable s, ElementOf s ~ Char)
+    => ParserPE s (InputError :+: ValidationError Char) Char
+digit = satisfy isDigit
+
+{- | Parse a positive 'Integer' in decimal format. -}
+positive
+    :: (HasPosition s, Splittable s, MonoFoldable (PrefixOf s), ElementOf s ~ Char, ElementOf (PrefixOf s) ~ Char)
+    => ParserPE s (InputError :+: ValidationError (PrefixOf s)) Integer
+positive = do
+        digits <- atLeastOneWith isDigit
+        let
+             n = fromIntegral $ monolength digits
+             xs = zip [n - 1, n - 2 .. 0] (monotoList digits)
+        pure $ foldl' (+) 0 [ value d i | (i, d) <- xs]
+    where
+        value :: Char -> Integer -> Integer
+        -- Returns implementation-dependent garbage for non-decimal digits.
+        value c n = fromIntegral (ord c - ord '0') *  10 ^ n
+
+{- | Transform an 'Integer' parser into an 'Integer' parser for signed numbers. -}
+signed
+    :: (HasPosition s, Streamable s, ElementOf s ~ Char)
+    => ParserPE s (InputError :+: ValidationError (PrefixOf s)) Integer
+    -> ParserPE s (InputError :+: ValidationError (PrefixOf s)) Integer
+signed p = do
+    sgn <- first absurd (fromMaybe Positive <$> Parsers.maybe sign)
+    number <- p
+    case sgn of
+        Positive -> pure number
+        Negative -> pure (-number)
+
+{- | Parse a line comment.
+
+A line comment starts with @p@ and then runs to the end of the line (character @'\\n'@), returning
+the prefix in-between.
+
+note(s):
+
+    * If the streamable contains Windows end of lines, then the comment text will contain an ending
+    @'\\r'@. This can only be stripped by assuming more about @'PrefixOf' s@ (the practical
+    solution) or complicating the implementation.
+-}
+comment
+    :: (HasPosition s, Splittable s, ElementOf s ~ Char)
+    => ParserPE s e (PrefixOf s)        -- ^ Parser for beginning comment prefix.
+    -> ParserPE s e (PrefixOf s)
+comment p = do
+    _ <- p
+    first absurd $ takePrefixWith (/= '\n') <* Parsers.maybe cr
---
+++ tests/Tests/Parsers/CharSpec.hs
@@ -0,0 +1,229 @@
+module Tests.Parsers.CharSpec (
+    -- * Tests.
+    spec,
+) where
+ 
+-- Imports.
+-- Testing library.
+import Test.Hspec (Spec, describe, it)
+
+-- Testing helpers.
+import Lib.Runners
+
+-- Module to test.
+import Trisagion.Parsers.Char (Sign (..), lf, cr, spaces, notSpaces, sign, positive, signed)
+
+-- Package.
+import Trisagion.Parser (InputError (..))
+import Trisagion.Parsers.ParseError (ValidationError (..))
+
+
+-- Main module test driver.
+spec :: Spec
+spec = describe "Trisagion.Parsers.Char tests" $ do
+    spec_lf
+    spec_cr
+    spec_spaces
+    spec_notSpaces
+    spec_sign
+    spec_positive
+    spec_signed
+
+
+-- Tests.
+spec_lf :: Spec
+spec_lf = describe "lf tests" $ do
+    it "Success case" $ do
+        testSuccess
+            lf
+            "\n123"
+            '\n'
+            1
+
+    it "Failure case with non-matching element" $ do
+        testError
+            lf
+            "0123"
+            (Right $ ValidationError '0')
+            0
+
+    it "Failure case on end of input" $ do
+        testError
+            lf
+            ""
+            (Left $ InputError 1)
+            0
+
+spec_cr :: Spec
+spec_cr = describe "cr tests" $ do
+    it "Success case" $ do
+        testSuccess
+            cr
+            "\r123"
+            '\r'
+            1
+
+    it "Failure case with non-matching element" $ do
+        testError
+            cr
+            "0123"
+            (Right $ ValidationError '0')
+            0
+
+    it "Failure case on end of input" $ do
+        testError
+            cr
+            ""
+            (Left $ InputError 1)
+            0
+
+spec_spaces :: Spec
+spec_spaces = describe "spaces tests" $ do
+    it "Success cases" $ do
+        testSuccess
+            spaces
+            "  0123"
+            "  "
+            2
+
+        testSuccess
+            spaces
+            "  \t 0123"
+            "  \t "
+            4
+
+    it "Success cases with chars other than space and tab" $ do
+        testSuccess
+            spaces
+            "\v\f\r\n0123"
+            "\v\f\r\n"
+            4
+
+    it "No leading whitespace" $ do
+        testSuccess
+            spaces
+            "0123"
+            ""
+            0
+
+    it "Case of no input" $ do
+        testSuccess
+            spaces
+            ""
+            ""
+            0
+
+spec_notSpaces :: Spec
+spec_notSpaces = describe "notSpaces tests" $ do
+    it "Success case" $ do
+        testSuccess
+            notSpaces
+            "0123"
+            "0123"
+            4
+
+    it "Success on leading whitespace" $ do
+        testSuccess
+            notSpaces
+            "    0123"
+            ""
+            0
+
+    it "Case of no input" $ do
+        testSuccess
+            notSpaces
+            ""
+            ""
+            0
+
+spec_sign :: Spec
+spec_sign = describe "sign tests" $ do
+    it "Success cases" $ do
+        testSuccess
+            sign
+            "+123"
+            Positive
+            1
+
+        testSuccess
+            sign
+            "-123"
+            Negative
+            1
+
+    it "Failed validation" $ do
+        testError
+            sign
+            "0123"
+            (Right $ ValidationError '0')
+            0
+
+    it "Failure case on end of input" $ do
+        testError
+            sign
+            ""
+            (Left $ InputError 1)
+            0
+
+spec_positive :: Spec
+spec_positive = describe "positive tests" $ do
+    it "Success cases" $ do
+        testSuccess
+            positive
+            "123"
+            123
+            3
+
+        testSuccess
+            positive
+            "1abc"
+            1
+            1
+
+    it "Leading zeros" $ do
+        testSuccess
+            positive
+            "00123"
+            123
+            5
+
+    it "Failed validation" $ do
+        testError
+            positive
+            "abc"
+            (Right $ ValidationError "")
+            0
+
+    it "Failure case on end of input" $ do
+        testError
+            positive
+            ""
+            (Left InsufficientInputError)
+            0
+
+spec_signed :: Spec
+spec_signed = describe "signed tests" $ do
+    it "Success cases" $ do
+        testSuccess
+            (signed positive)
+            "123"
+            123
+            3
+
+        testSuccess
+            (signed positive)
+            "00123"
+            123
+            5
+
+        testSuccess
+            (signed positive)
+            "+123"
+            123
+            4
+
+        testSuccess
+            (signed positive)
+            "-123"
+            (-123)
+            4
