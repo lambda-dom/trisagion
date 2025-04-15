ceeaf59bf7df150a237f5cc64e89785db43f2f7b
lambda-dom <grodrigues.math@gmail.com>
2025-04-15 10:50:27 +0100

    Lazy `ByteString` and `Text` instances.

--- src/Trisagion/Typeclasses/Builder.hs
+++ src/Trisagion/Typeclasses/Builder.hs
@@ -11,8 +11,15 @@ module Trisagion.Typeclasses.Builder (
 
 -- Imports.
 -- Base.
-import Data.Foldable (foldMap')
+import Data.Foldable (foldMap', toList)
 import Data.Kind (Type)
+import Data.Word (Word8)
+
+-- Libraries.
+import qualified Data.ByteString.Lazy as LazyBytes (ByteString)
+import qualified Data.ByteString.Builder as Bytes (Builder, toLazyByteString, word8)
+import qualified Data.Text.Lazy as LazyText (Text)
+import qualified Data.Text.Lazy.Builder as Text (Builder, toLazyText, singleton, fromString)
 
 -- non-Hackage libraries.
 import Data.MonoFunctor (ElementOf)
@@ -37,3 +44,26 @@ class (Monoid m, Streamable (BuilderOf m)) => Builder m where
     {- | Build from a foldable of many @'ElementOf' ('BuilderOf' m)@. -}
     many :: Foldable t => t (ElementOf (BuilderOf m)) -> m
     many = foldMap' one
+
+
+-- Instances.
+instance Builder Bytes.Builder where
+    type BuilderOf Bytes.Builder = LazyBytes.ByteString
+
+    unpack :: Bytes.Builder -> LazyBytes.ByteString
+    unpack = Bytes.toLazyByteString
+
+    one :: Word8 -> Bytes.Builder
+    one = Bytes.word8
+
+instance Builder Text.Builder where
+    type BuilderOf Text.Builder = LazyText.Text
+
+    unpack :: Text.Builder -> LazyText.Text
+    unpack = Text.toLazyText
+
+    one :: Char -> Text.Builder
+    one = Text.singleton
+
+    many :: Foldable t => t Char -> Text.Builder
+    many = Text.fromString . toList
