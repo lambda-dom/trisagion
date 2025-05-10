0df387dd05acca73fd7f6a0a2b75e866eca37978
lambda-dom <grodrigues.math@gmail.com>
2025-05-10 19:00:17 +0100

    `Applicative` and `Monad` instances.

--- src/Trisagion/Parser.hs
+++ src/Trisagion/Parser.hs
@@ -45,6 +45,48 @@ instance Bifunctor (Parser s) where
     bimap :: (d -> e) -> (a -> b) -> Parser s d a -> Parser s e b
     bimap g f p = Parser $ bimap g f . run p
 
+{- | The 'Applicative' instance.
+
+Allows sequencing of parsers and combine their results. With @pure x@ values can be embedded in a
+parser. The parser @p \<*\> q@ first runs @p@ then @q@, and returns the result of @p@ applied to
+the result of @q@.
+
+note(s):
+
+  * The parser @p \<*\> q@ short-circuits on @p@ erroring out, that is, @q@ never runs.
+-}
+instance Applicative (Parser s e) where
+    {-# INLINE pure #-}
+    pure :: a -> Parser s e a
+    pure x = Parser $ \ s -> Success x s
+
+    {-# INLINE (<*>) #-}
+    (<*>) :: Parser s e (a -> b) -> Parser s e a -> Parser s e b
+    (<*>) p q = Parser $ \ xs ->
+        case run p xs of
+            Error e1     -> Error e1
+            Success f ys ->
+                case run q ys of
+                    Error e2     -> Error e2
+                    Success x zs -> Success (f x) zs
+
+{- | The 'Monad' instance.
+
+The bind combinator @p >>= f@ first runs @p@ and then the parser obtained by applying the monadic
+function @f@ to the result.
+
+note(s):
+
+  * As with @p \<*\> q@, @p >>= f@ short-circuits on @p@ erroring out.
+-}
+instance Monad (Parser s e) where
+    {-# INLINE (>>=) #-}
+    (>>=) :: Parser s e a -> (a -> Parser s e b) -> Parser s e b
+    (>>=) p h = Parser $ \ xs ->
+        case run p xs of
+            Error e      -> Error e
+            Success x ys -> run (h x) ys
+
 
 {- | Run the parser on the input and return the results. -}
 {-# INLINE run #-}
