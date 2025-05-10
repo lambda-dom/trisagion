bcb6bd0a9173a4f387d9cc7769e05c0d05409243
lambda-dom <grodrigues.math@gmail.com>
2025-05-10 18:46:30 +0100

    `Result` type and `Parser` monad.

--- src/Trisagion/Parser.hs
+++ src/Trisagion/Parser.hs
@@ -6,5 +6,62 @@ The @Parser@ monad.
 
 module Trisagion.Parser (
     -- * Type operators.
+    (:+:),
+    (:*:),
+
+    -- * The parsing monad.
+    Parser,
+
+    -- ** Basic functions.
+    parse,
+    eval,
+    remainder,
 ) where
 
+-- Imports.
+-- Base.
+import Data.Bifunctor (Bifunctor (..))
+
+-- Package.
+import Trisagion.Types.Result (Result (..), toEither, withResult)
+
+
+{- | Right-associative type operator version of the 'Either' type constructor. -}
+type (:+:) = Either
+infixr 6 :+:
+
+{- | Right-associative type operator version of the @(,)@ tuple type constructor. -}
+type a :*: b = (a, b)
+infixr 6 :*:
+
+
+{- | The parsing monad. -}
+newtype Parser s e a = Parser (s -> Result s e a)
+    deriving stock Functor
+
+{- | The 'Bifunctor' instance, providing functoriality in the error type. -}
+instance Bifunctor (Parser s) where
+    {-# INLINE bimap #-}
+    bimap :: (d -> e) -> (a -> b) -> Parser s d a -> Parser s e b
+    bimap g f p = Parser $ bimap g f . run p
+
+
+{- | Run the parser on the input and return the results. -}
+{-# INLINE run #-}
+run :: Parser s e a -> s -> Result s e a
+run (Parser p) = p
+
+{- | Parse the input and return the results. -}
+{-# INLINE parse #-}
+parse :: Parser s e a -> s -> e :+: (a :*: s)
+parse p = toEither . run p
+
+{- | Evaluate the parser on the input and return the result, discarding the remainder. -}
+{-# INLINE eval #-}
+eval :: Parser s e a -> s -> e :+: a
+eval p = withResult Left (\ x _ -> Right x) . run p
+
+{- | Run the parser on the input and return the remainder, discarding the parsed value. -}
+{-# INLINE remainder #-}
+remainder :: Parser s e a -> s -> e :+: s
+remainder p =  withResult Left (\ _ xs -> Right xs) . run p
