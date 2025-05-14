9c1f4fea8efec0364f6857e30b1ddac0c86e19bd
lambda-dom <grodrigues.math@gmail.com>
2025-05-14 20:50:42 +0100

    `Splittable` parsers.

---
+++ src/Trisagion/Parsers/Splittable.hs
@@ -0,0 +1,68 @@
+{- |
+Module: Trisagion.Parsers.Splittable
+
+Parsers with @'Splittable' s@ constraints on the input stream @s@.
+
+Implementations requiring the computation of the length of a prefix have a @'MonoFoldable' s@
+constraint. This can be important for performance because for a `Splittable` like @Text@ this is
+@O(n)@, while for other types like lazy bytestrings, it forces the entirety of the value into
+memory which is probably not what is desired.
+-}
+
+module Trisagion.Parsers.Splittable (
+    -- * Parsers with a @'Splittable' s => 'Parser' s e a@ constraint.
+    takeExact,
+    match,
+) where
+
+-- Imports.
+-- Base.
+import Data.Bifunctor (Bifunctor (..))
+import Data.Void (Void, absurd)
+
+-- Libraries.
+import Control.Monad.Except (MonadError (..))
+import Optics ((%), review)
+
+-- non-Hackage libraries.
+import Mono.Typeclasses.MonoFoldable (MonoFoldable (..))
+
+-- Package.
+import Trisagion.Types.ErrorItem (endOfInput)
+import Trisagion.Types.ParseError (ParseError, singleton)
+import Trisagion.Typeclasses.Splittable (Splittable (..))
+import Trisagion.Parser (Parser, takePrefix)
+import Trisagion.Parsers.ParseError (ValidationError, validate)
+
+
+{- | Parse an exact, fixed size prefix.
+
+note(s):
+
+    * Implementation requires computing the length of the prefix.
+-}
+{-# INLINE takeExact #-}
+takeExact
+    :: (Splittable s, MonoFoldable (PrefixOf s))
+    => Word -> Parser s (ParseError s Void) (PrefixOf s)
+takeExact n = do
+    prefix <- first absurd $ takePrefix n
+    if monolength prefix /= n
+        then throwError $ review (singleton % endOfInput) n
+        else pure prefix
+
+
+{- | Parse a matching prefix.
+
+note(s):
+
+    * The implementation requires computing the length of the argument prefix.
+-}
+{-# INLINE match #-}
+match
+    :: (Splittable s, Eq (PrefixOf s), MonoFoldable (PrefixOf s))
+    => PrefixOf s                       -- ^ Matching prefix.
+    -> Parser s (ParseError s (ValidationError (PrefixOf s))) (PrefixOf s)
+match xs = first (fmap (either id absurd)) $ validate v (takeExact (monolength xs))
+    where
+        v prefix = if xs == prefix then Right prefix else Left $ pure prefix
