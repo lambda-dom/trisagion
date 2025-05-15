00b0c4a27a96bc3562475db411f2e1eb8c68dddc
lambda-dom <grodrigues.math@gmail.com>
2025-05-15 16:23:20 +0100

    `ParseError` type.

--- src/Trisagion/Types/ParseError.hs
+++ src/Trisagion/Types/ParseError.hs
@@ -6,5 +6,25 @@ The @ParseError@ error type.
 
 module Trisagion.Types.ParseError (
     -- * The @ParseError@ type.
+    ParseError (..)
 ) where
 
+-- Imports.
+-- Base.
+import Data.Typeable (Typeable)
+
+-- Package.
+import Trisagion.Types.ErrorItem (ErrorItem)
+
+
+{- | The 'ParseError' type. -}
+data ParseError s e where
+    -- | The Nil constructor.
+    Nil :: ParseError s e
+
+    -- | The Cons constructor.
+    Cons :: (Typeable d, Eq d, Show d) => !(ErrorItem s e) -> [ErrorItem s d] -> ParseError s e
+
+-- Instances.
+deriving stock instance Functor (ParseError s)
+deriving stock instance (Show s, Show e) => Show (ParseError s e)
