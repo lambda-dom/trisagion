ecdc818d03aafa8507767412698f996209da720d
lambda-dom <grodrigues.math@gmail.com>
2025-05-17 11:44:52 +0100

    Use offsets in `ParseError`.

--- src/Trisagion/Types/ParseError.hs
+++ src/Trisagion/Types/ParseError.hs
@@ -20,13 +20,13 @@ module Trisagion.Types.ParseError (
 
 -- Imports.
 -- Base.
-import Data.Bifunctor (Bifunctor (..))
 import Data.Typeable (Typeable)
 
 -- Libraries.
 import Optics.Core (Prism', (%), prism', review)
 
 -- Package.
+import Trisagion.Typeclasses.HasOffset (HasOffset (..))
 import Trisagion.Types.ErrorItem (ErrorItem, TraceItem, endOfInput, errorItem, traceItem)
 
 
@@ -35,33 +35,27 @@ import Trisagion.Types.ErrorItem (ErrorItem, TraceItem, endOfInput, errorItem, t
 Functionally, a @'ParseError' s e@ is a @'ErrorItem' s e@, the top of the entire error trace, and a
 list of @forall d . 'ErrorItem' s d@ wrapped in a @'TraceItem' s@.
 -}
-data ParseError s e where
+data ParseError e where
     -- | The Nil constructor.
-    Nil :: ParseError s e
+    Nil :: ParseError e
 
     -- | The Cons constructor.
-    Cons :: !(ErrorItem s e) -> [TraceItem s] -> ParseError s e
+    Cons :: !(ErrorItem e) -> [TraceItem] -> ParseError e
 
 -- Instances.
-deriving stock instance (Eq s, Eq e) => Eq (ParseError s e)
-deriving stock instance (Show s, Show e) => Show (ParseError s e)
-deriving stock instance Functor (ParseError s)
+deriving stock instance Eq e => Eq (ParseError e)
+deriving stock instance Show e => Show (ParseError e)
+deriving stock instance Functor ParseError
 
-instance Bifunctor ParseError where
-    {-# INLINE bimap #-}
-    bimap :: (s -> t) -> (d -> e) -> ParseError s d -> ParseError t e
-    bimap _ _ Nil         = Nil
-    bimap f g (Cons e es) = Cons (bimap f g e) (fmap (fmap f) es)
-
-instance Semigroup (ParseError s e) where
+instance Semigroup (ParseError e) where
     {-# INLINE (<>) #-}
-    (<>) :: ParseError s e -> ParseError s e -> ParseError s e
+    (<>) :: ParseError e -> ParseError e -> ParseError e
     (<>) Nil x = x
     (<>) x   _ = x
 
-instance Monoid (ParseError s e) where
+instance Monoid (ParseError e) where
     {-# INLINE mempty #-}
-    mempty :: ParseError s e
+    mempty :: ParseError e
     mempty = Nil
 
 {- | Prism for the nil value.
@@ -69,53 +63,53 @@ instance Monoid (ParseError s e) where
 The nil value can also be constructed by 'mempty'.
 -}
 {-# INLINE nil #-}
-nil :: Prism' (ParseError s e) ()
+nil :: Prism' (ParseError e) ()
 nil = prism' construct match
     where
-        construct :: () -> ParseError s e
+        construct :: () -> ParseError e
         construct _ = Nil
 
-        match :: ParseError s e -> Maybe ()
+        match :: ParseError e -> Maybe ()
         match Nil = Just ()
         match _   = Nothing
 
 {- | The singleton prism for 'ParseError' values with no backtrace. -}
 {-# INLINE singleton #-}
-singleton :: Prism' (ParseError s e) (ErrorItem s e)
+singleton :: Prism' (ParseError e) (ErrorItem e)
 singleton = prism' construct match
     where
-        construct :: ErrorItem s e -> ParseError s e
+        construct :: ErrorItem e -> ParseError e
         construct e = Cons e []
 
-        match :: ParseError s e -> Maybe (ErrorItem s e)
+        match :: ParseError e -> Maybe (ErrorItem e)
         match (Cons e []) = Just e
         match _           = Nothing
 
 {- | The cons prism for 'ParseError' values with a backtrace. -}
 {-# INLINE cons #-}
-cons :: Prism' (ParseError s e) (ErrorItem s e, [TraceItem s])
+cons :: Prism' (ParseError e) (ErrorItem e, [TraceItem])
 cons = prism' construct match
     where
-        construct :: (ErrorItem s e, [TraceItem s]) -> ParseError s e
+        construct :: (ErrorItem e, [TraceItem]) -> ParseError e
         construct = uncurry Cons
 
-        match :: ParseError s e -> Maybe (ErrorItem s e, [TraceItem s])
+        match :: ParseError e -> Maybe (ErrorItem e, [TraceItem])
         match (Cons e es) = Just (e, es)
         match _           = Nothing
 
 
 {- | Constructor helper to build an end of input 'ParseError'. -}
 {-# INLINE makeEOI #-}
-makeEOI :: Word -> ParseError s e
+makeEOI :: Word -> ParseError e
 makeEOI = review (singleton % endOfInput)
 
 {- | Constructor helper to build a 'ParseError' with a backtrace. -}
 {-# INLINE makeBacktrace #-}
 makeBacktrace
-    :: (Typeable d, Eq d, Show d)
+    :: (HasOffset s, Typeable d, Eq d, Show d)
     => s                                -- ^ Input stream.
     -> e                                -- ^ Error tag.
-    -> ParseError s d                   -- ^ Backtrace.
-    -> ParseError s e
-makeBacktrace xs e Nil         = review (singleton % errorItem) (xs, e)
-makeBacktrace xs e (Cons y ys) = Cons (review errorItem (xs, e)) (review traceItem y : ys)
+    -> ParseError d                     -- ^ Backtrace.
+    -> ParseError e
+makeBacktrace xs e Nil         = review (singleton % errorItem) (offset xs, e)
+makeBacktrace xs e (Cons y ys) = Cons (review errorItem (offset xs, e)) (review traceItem y : ys)
