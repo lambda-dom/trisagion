66f1a5a581a7177a49740e32bde92ce0ff614765
lambda-dom <grodrigues.math@gmail.com>
2025-06-06 18:55:12 +0100

    Add missing `Splittable` parsers.

--- src/Trisagion/Parsers/Splittable.hs
+++ src/Trisagion/Parsers/Splittable.hs
@@ -9,7 +9,10 @@ module Trisagion.Parsers.Splittable (
     -- $splittable-parsers
     takePrefix,
     skipPrefix,
+    takeExact,
+    matchPrefix,
     takeWith,
+    takeWith1,
     skipWith,
     takeRemainder,
     isolate,
@@ -24,12 +27,20 @@ import Data.Void (Void, absurd)
 
 -- Libraries.
 import Control.Monad.State (MonadState (..), gets, modify)
+import Optics.Core ((%), review)
+
+-- non-Hackage libraries.
+import Mono.Typeclasses.MonoFunctor (ElementOf)
+import Mono.Typeclasses.MonoFoldable (MonoFoldable (..))
 
 -- Package.
 import Trisagion.Typeclasses.HasOffset (HasOffset (..))
 import Trisagion.Typeclasses.Splittable (Splittable (..))
-import Trisagion.Parser (Parser, eval, throw)
-import Mono.Typeclasses.MonoFunctor (ElementOf)
+import Trisagion.Types.ErrorItem (endOfInput)
+import Trisagion.Types.ParseError (ParseError, ValidationError, singleton)
+import Trisagion.Parser (Parser, eval, throw, lookAhead)
+import Trisagion.Parsers.Streamable (InputError, satisfy)
+import Trisagion.Parsers.ParseError (throwParseError)
 
 
 {- $splittable-parsers
@@ -71,6 +82,56 @@ Right ((),"")
 skipPrefix :: Splittable s => Word -> Parser s Void ()
 skipPrefix n = modify (dropPrefix n)
 
+{- | Parse an exact, fixed size prefix.
+
+note(s):
+
+    * Implementation requires computing the length of the prefix.
+
+=== __Examples:__
+
+>>> parse (takeExact 2) "0123"
+Right ("01","23")
+
+>>> parse (takeExact 10) "0123"
+Left (Cons (EndOfInput 10) [])
+-}
+{-# INLINE takeExact #-}
+takeExact
+    :: (Splittable s, MonoFoldable (PrefixOf s))
+    => Word -> Parser s InputError (PrefixOf s)
+takeExact n = do
+    prefix <- first absurd $ takePrefix n
+    if monolength prefix /= n
+        then throw $ review (singleton % endOfInput) n
+        else pure prefix
+
+{- | Parse a matching prefix.
+
+note(s):
+
+    * The implementation requires computing the length of the argument prefix.
+
+=== __Examples:__
+
+>>> parse (matchPrefix "01") (initialize "0123")
+Right ("01",Counter 2 "23")
+
+>>> parse (matchPrefix "012345") (initialize "0123")
+Left (Cons (EndOfInput 6) [])
+
+>>> parse (matchPrefix "{}") (initialize "0123")
+Left (Cons (ErrorItem 2 (ValidationError "{}")) [])
+-}
+{-# INLINE matchPrefix #-}
+matchPrefix
+    :: (HasOffset s, Splittable s, Eq (PrefixOf s), MonoFoldable (PrefixOf s))
+    => PrefixOf s                       -- ^ Matching prefix.
+    -> Parser s (ParseError (ValidationError (PrefixOf s))) (PrefixOf s)
+matchPrefix xs = do
+    prefix <- first (fmap absurd) $ takeExact (monolength xs)
+    if xs == prefix then pure xs else throwParseError (pure xs)
+
 {- | Parse the longest prefix whose elements satisfy a predicate.
 
 === __Examples:__
@@ -86,6 +147,33 @@ takeWith :: Splittable s => (ElementOf s -> Bool) -> Parser s Void (PrefixOf s)
 takeWith p = do
     (prefix, rest) <- gets (splitWith p)
     put rest $> prefix
+
+{- | Parse the longest prefix with at least one element whose elements satisfy a predicate.
+
+=== __Examples:__
+
+>>> parse (takeWith1 ('0' ==)) (initialize "0123")
+Right ("0",Counter 1 "123")
+
+>>> parse (takeWith1 ('0' ==)) (initialize "0003")
+Right ("000",Counter 3 "3")
+
+>>> parse (takeWith1 ('1' ==)) (initialize "0123")
+Left (Cons (ErrorItem 1 (ValidationError '0')) [])
+
+>>> parse (takeWith1 ('0' ==)) (initialize "")
+Left (Cons (EndOfInput 1) [])
+-}
+{-# INLINE takeWith1 #-}
+takeWith1
+    :: (HasOffset s, Splittable s)
+    => (ElementOf s -> Bool)            -- ^ Predicate on @'ElementOf' s@.
+    -> Parser s (ParseError (ValidationError (ElementOf s))) (PrefixOf s)
+takeWith1 p = do
+    x <- first absurd $ lookAhead (satisfy p)
+    case x of
+        Left e  -> throw e
+        Right _ -> first absurd $ takeWith p
 
 {- | Drop the longest prefix whose elements satisfy a predicate.
 
@@ -113,7 +201,7 @@ takeRemainder = do
 The prefix on which the parser runs may have a size smaller than @n@ if there is not enough input
 in the stream. Any unconsumed input in the prefix is silently discarded. If such behavior is
 undesirable, 'Control.Monad.guard' the parser to run with an appropriate check -- see
-'Trisagion.Parser.ensureEOI'.
+'Trisagion.Parsers.Streamable.ensureEOI'.
 
 === __Examples:__
 
