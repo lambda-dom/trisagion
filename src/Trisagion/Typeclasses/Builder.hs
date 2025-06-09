0f53c7d1e335d0c755ebb78d850c0df2c71975a8
lambda-dom <grodrigues.math@gmail.com>
2025-06-09 16:14:30 +0100

    `Builder` instances.

--- src/Trisagion/Typeclasses/Builder.hs
+++ src/Trisagion/Typeclasses/Builder.hs
@@ -11,7 +11,17 @@ module Trisagion.Typeclasses.Builder (
 
 -- Imports.
 -- Base.
+import Data.Foldable (toList)
 import Data.Kind (Type)
+import Data.Word (Word8)
+
+-- Libraries.
+import Data.ByteString.Lazy (ByteString)
+import Data.ByteString.Builder (toLazyByteString, word8, lazyByteString)
+import qualified Data.ByteString.Builder as Bytes (Builder)
+import Data.Text.Lazy (Text)
+import Data.Text.Lazy.Builder (toLazyText, singleton, fromString, fromLazyText)
+import qualified Data.Text.Lazy.Builder as Text (Builder)
 
 -- non-Hackage libraries.
 import Mono.Typeclasses.MonoFunctor (ElementOf)
@@ -45,3 +55,38 @@ class (Monoid m, Streamable (BuilderOf m)) => Builder m where
     many :: Foldable t => t (ElementOf (BuilderOf m)) -> m
     many = foldMap one
 
+
+-- Instances.
+instance Builder Bytes.Builder where
+    type BuilderOf Bytes.Builder = ByteString
+
+    {-# INLINE unpack #-}
+    unpack :: Bytes.Builder -> ByteString
+    unpack = toLazyByteString
+
+    {-# INLINE pack #-}
+    pack :: ByteString -> Bytes.Builder
+    pack = lazyByteString
+
+    {-# INLINE one #-}
+    one :: Word8 -> Bytes.Builder
+    one = word8
+
+instance Builder Text.Builder where
+    type BuilderOf Text.Builder = Text
+
+    {-# INLINE unpack #-}
+    unpack :: Text.Builder -> Text
+    unpack = toLazyText
+
+    {-# INLINE pack #-}
+    pack :: Text -> Text.Builder
+    pack = fromLazyText
+
+    {-# INLINE one #-}
+    one :: Char -> Text.Builder
+    one = singleton
+
+    {-# INLINE many #-}
+    many :: Foldable t => t Char -> Text.Builder
+    many = fromString . toList
