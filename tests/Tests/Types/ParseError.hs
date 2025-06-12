0be5ef12176795036b9894c3cab4922df1a98095
lambda-dom <grodrigues.math@gmail.com>
2025-06-12 15:08:42 +0100

    `prop_fmap_monoid_morphism_unit` property.

--- tests/Tests/Types/ParseError.hs
+++ tests/Tests/Types/ParseError.hs
@@ -3,17 +3,23 @@
 module Tests.Types.ParseError (
     -- * Tests.
     tests,
+
+    -- * Properties.
+    prop_fmap_monoid_morphism_unit,
 ) where
 
 -- Imports.
 -- Base.
+import Data.Bifunctor (Bifunctor(..))
+import Data.List.NonEmpty (NonEmpty (..))
 import Data.Maybe (fromMaybe)
+import Data.Word (Word8)
 
 -- Libraries.
 import Optics.Core ((%), review)
 
 -- Testing.
-import Hedgehog (Gen, Group (..), property, checkParallel)
+import Hedgehog (Gen, Group (..), property, checkParallel, PropertyT, forAll)
 import qualified Hedgehog.Range as Range (linearBounded)
 import qualified Hedgehog.Gen as Gen (word8, maybe)
 
@@ -22,12 +28,14 @@ import Trisagion.Types.ErrorItem (errorItem)
 import Trisagion.Types.ParseError (ParseError, singleton)
 
 -- Package testing helpers.
+import Lib.Functions (Function, genFunction, fromFunction)
 import Lib.Generators (genSize)
 import Lib.Properties (
     prop_monoid_left_identity,
     prop_monoid_right_identity,
     prop_monoid_associativity,
     prop_monoid_idempotency,
+    prop_monoid_morphism_unit,
     )
 
 
@@ -39,13 +47,25 @@ genParseError gen = fromMaybe mempty <$> Gen.maybe genError
         genError = makeError <$> genSize <*> gen
 
 -- Properties.
--- prop_monoidMorphism_Unit :: (Eq e, Show e) => (e -> e -> e) -> Gen e -> Property
--- prop_monoidMorphism_Unit h gen = property $ do
---     -- Needed for inference.
---     let unit = mempty :: ParseError e
+prop_fmap_monoid_morphism_unit
+    :: forall m e . (Monad m, Show e, Ord e)
+    => Gen e
+    -> PropertyT m ()
+prop_fmap_monoid_morphism_unit gen = do
+        f <- forAll genF
+        prop_monoid_morphism_unit (transf f)
+    where
+        genF :: Gen (Function e e)
+        genF = genFunction gen gen
 
---     f <- forAll (genFunctionExp gen)
---     fmap (makeFunction h f) unit === unit
+        combine :: (b -> b -> b) -> (a -> b) -> (a -> b) -> (a -> b)
+        combine h f g = uncurry h . bimap f g . (\ x -> (x, x))
+
+        nat :: Function e e -> e -> e
+        nat = fromFunction (id :| []) ((.) :| [combine min, combine max])
+
+        transf :: Function e e -> ParseError e -> ParseError e
+        transf g = fmap (nat g)
 
 -- prop_monoidMorphism_Mult :: (Eq e, Show e) => (e -> e -> e) -> Gen e -> Property
 -- prop_monoidMorphism_Mult h gen = property $ do
@@ -58,10 +78,15 @@ genParseError gen = fromMaybe mempty <$> Gen.maybe genError
 -- Main module test driver.
 tests :: IO Bool
 tests = checkParallel $ Group "Tests.Types.ParseError" [
-        ("Left monoid identity", property $ prop_monoid_left_identity gen),
-        ("Right monoid identity", property $ prop_monoid_right_identity gen),
-        ("Associativity", property $ prop_monoid_associativity gen),
-        ("Idempotency", property $ prop_monoid_idempotency gen)
+        ("Left monoid identity", property $ prop_monoid_left_identity genP),
+        ("Right monoid identity", property $ prop_monoid_right_identity genP),
+        ("Associativity", property $ prop_monoid_associativity genP),
+        ("Idempotency", property $ prop_monoid_idempotency genP),
+        ("Monoid morphism for functor map: unit", property $ prop_fmap_monoid_morphism_unit genW)
         ]
     where
-        gen = genParseError $ Gen.word8 Range.linearBounded
+        genW :: Gen Word8
+        genW = Gen.word8 Range.linearBounded
+
+        genP :: Gen (ParseError Word8)
+        genP = genParseError genW
