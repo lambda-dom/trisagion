f13d62f9e3ff7c3c13d646ccc00f6f0d6b870028
lambda-dom <grodrigues.math@gmail.com>
2025-06-15 02:19:12 +0100

    `Parser` tests.

--- tests/Lib/Properties/Alternative.hs
+++ tests/Lib/Properties/Alternative.hs
@@ -11,21 +11,23 @@ module Lib.Properties.Alternative (
     prop_alternative_left_catch,
     prop_alternative_left_absorption,
     prop_alternative_left_zero,
+    prop_alternative_left_distributivity,
 
     -- * Property groups.
     alternativeLaws,
+    distributiveLaw,
 ) where
 
 -- Imports.
 -- Base.
 import Control.Applicative (Alternative (..))
+import Data.Bifunctor (Bifunctor (..))
 import Data.List.NonEmpty (NonEmpty (..))
-import Data.Word (Word32)
+import Data.Void (absurd)
+import Data.Word (Word8)
 
 -- Testing library.
-import Hedgehog (Gen, PropertyT, forAll, Property, property)
-import qualified Hedgehog.Gen as Gen (list)
-import qualified Hedgehog.Range as Range (linear)
+import Hedgehog (Gen, PropertyT, Property, forAll, property)
 
 -- non-Hackage libraries.
 import Mono.Typeclasses.MonoFunctor (ElementOf)
@@ -33,13 +35,16 @@ import Mono.Typeclasses.MonoFunctor (ElementOf)
 -- Package.
 import Trisagion.Typeclasses.HasOffset (HasOffset)
 import Trisagion.Types.ParseError (ParseError)
-import Trisagion.Streams.Counter (initialize, Counter)
 import Trisagion.Parser (Parser)
-import Trisagion.Parsers.Combinators (pairWith)
+import Trisagion.Parsers.ParseError (throwParseError)
+import Trisagion.Parsers.Combinators (pairWith, count)
+import Trisagion.Parsers.Streamable (one)
 
 -- Package testing.
 import Lib.ParserExp (ParserExp, fromParserExp, parserExps)
 import Lib.Property (prop_parser_extensional_equality)
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
 
 
 -- ParserExp conversion function.
@@ -48,6 +53,17 @@ nat = fromParserExp id unaries binaries
     where
         unaries = id :| []
         binaries = (<|>) :| [(*>), (<*), pairWith min, pairWith max]
+
+
+-- Parsers.
+parseFunction :: (HasOffset s, ElementOf s ~ Word8) => Parser s (ParseError Word8) (Word8 -> Word8)
+parseFunction = do
+    x <- first (fmap absurd) one
+    case x `rem` 4 of
+        0 -> throwParseError x
+        1 -> first (fmap absurd) one >>= \ n -> bimap (fmap absurd) (const (n *)) $ count (fromIntegral n) one
+        2 -> first (fmap absurd) one >>= \ n -> pure (max n)
+        _ -> pure id
 
 
 {- | Left identity for the 'Alternative' instance. -}
@@ -68,7 +84,7 @@ prop_alternative_left_identity elems streams = do
 
 {- | Left identity for the 'Alternative' instance. -}
 prop_alternative_right_identity
-    :: forall m s . (Monad m, HasOffset s, Eq s, Show s, Ord (ElementOf s), Show (ElementOf s))
+    :: (Monad m, HasOffset s, Eq s, Show s, Ord (ElementOf s), Show (ElementOf s))
     => Gen (ElementOf s)
     -> Gen s
     -> PropertyT m ()
@@ -81,7 +97,7 @@ prop_alternative_right_identity elems streams = do
 
 {- | Associativity for the 'Alternative' instance. -}
 prop_alternative_associativity
-    :: forall m s . (Monad m, HasOffset s, Eq s, Show s, Ord (ElementOf s), Show (ElementOf s))
+    :: (Monad m, HasOffset s, Eq s, Show s, Ord (ElementOf s), Show (ElementOf s))
     => Gen (ElementOf s)
     -> Gen s
     -> PropertyT m ()
@@ -96,7 +112,7 @@ prop_alternative_associativity elems streams = do
 
 {- | Left catch for the 'Alternative' instance. -}
 prop_alternative_left_catch
-    :: forall m s . (Monad m, HasOffset s, Eq s, Show s, Ord (ElementOf s), Show (ElementOf s))
+    :: (Monad m, HasOffset s, Eq s, Show s, Ord (ElementOf s), Show (ElementOf s))
     => Gen (ElementOf s)
     -> Gen s
     -> PropertyT m ()
@@ -125,7 +141,7 @@ prop_alternative_left_absorption elems streams = do
 
 {- | Left zero for the 'Alternative' instance. -}
 prop_alternative_left_zero
-    :: forall m s . (Monad m, HasOffset s, Eq s, Show s, Ord (ElementOf s), Show (ElementOf s))
+    :: (Monad m, HasOffset s, Eq s, Show s, Ord (ElementOf s), Show (ElementOf s))
     => Gen (ElementOf s)
     -> Gen s
     -> PropertyT m ()
@@ -136,13 +152,30 @@ prop_alternative_left_zero elems streams = do
         empty
         streams
 
+{- | Left distributivity. -}
+prop_alternative_left_distributivity
+    :: (Monad m, HasOffset s, ElementOf s ~ Word8, Eq s, Show s)
+    => Gen s
+    -> PropertyT m ()
+prop_alternative_left_distributivity streams = do
+        let f = parseFunction
+        x <- nat <$> forAll (parserExps elems elems)
+        y <- nat <$> forAll (parserExps elems elems)
+        prop_parser_extensional_equality
+            (f <*> (x <|> y))
+            ((f <*> x) <|> (f <*> y))
+            streams
+    where
+        elems :: Gen Word8
+        elems = Gen.word8 Range.constantBounded
+
 {- | 'Alternative' instance laws property group. -}
 alternativeLaws
-    :: forall s . (Ord (ElementOf s), Show (ElementOf s))
-    => Word32
-    -> Gen (ElementOf s)
+    :: (HasOffset s, Eq s, Show s, Ord (ElementOf s), Show (ElementOf s))
+    => Gen (ElementOf s)
+    -> Gen s
     -> [(String, Property)]
-alternativeLaws n elems = fmap (property <$>)
+alternativeLaws elems streams = fmap (property <$>)
         [
             ("Left identity", prop_alternative_left_identity elems streams),
             ("Right identity", prop_alternative_right_identity elems streams),
@@ -151,6 +184,10 @@ alternativeLaws n elems = fmap (property <$>)
             ("Left absorption", prop_alternative_left_absorption elems streams),
             ("Left zero", prop_alternative_left_zero elems streams)
         ]
-    where
-        streams :: Gen (Counter [ElementOf s])
-        streams = initialize <$> Gen.list (Range.linear 0 (fromIntegral n)) elems
+
+distributiveLaw
+    :: (HasOffset s, ElementOf s ~ Word8, Eq s, Show s)
+    => Gen s
+    -> [(String, Property)]
+distributiveLaw streams = fmap (property <$>)
+    [("Left distributivity", prop_alternative_left_distributivity streams)]
