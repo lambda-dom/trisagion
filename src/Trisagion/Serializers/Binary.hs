201b99ffd1939c063a340325d41fe76791cfe7fe
lambda-dom <grodrigues.math@gmail.com>
2025-07-14 17:49:23 +0100

    Serializer combinators.

--- src/Trisagion/Serializers/Binary.hs
+++ src/Trisagion/Serializers/Binary.hs
@@ -8,18 +8,47 @@ module Trisagion.Serializers.Binary (
     -- * Serializers @'Binary' m => 'Serializer' m a@.
     word8,
     int8,
+
+    -- * Little-endian serializers.
+    word16Le,
+    word32Le,
+    word64Le,
+    int16Le,
+    int32Le,
+    int64Le,
+
+    -- * Big-endian serializers.
+    word16Be,
+    word32Be,
+    word64Be,
+    int16Be,
+    int32Be,
+    int64Be,
+
+    -- * List to binary serializers.
+    sizedOf,
+
+    -- * Text to binary serializers.
+    char,
+    string,
 ) where
 
 -- Imports.
 -- Base.
+import Data.Char (ord)
 import Data.Functor.Contravariant (Contravariant (..))
-import Data.Int (Int8)
-import Data.Word (Word8)
+import Data.Int (Int8, Int16, Int32, Int64)
+import Data.Word (Word8, Word16, Word32, Word64)
 
 -- Package.
 import Trisagion.Typeclasses.Builder (one)
 import Trisagion.Typeclasses.Binary (Binary)
+import qualified Trisagion.Typeclasses.Binary as Binary (word16Le, word32Le, word64Le)
+import qualified Trisagion.Typeclasses.Binary as Binary (word16Be, word32Be, word64Be)
+import qualified Trisagion.Typeclasses.Binary as Binary (int16Le, int32Le, int64Le)
+import qualified Trisagion.Typeclasses.Binary as Binary (int16Be, int32Be, int64Be)
 import Trisagion.Serializer (Serializer, embed)
+import Trisagion.Serializers.Combinators (listOf)
 
 
 {- | Serialize a 'Word8'. -}
@@ -29,3 +58,71 @@ word8 = embed one
 {- | Serialize an 'Int8'. -}
 int8 :: Binary m => Serializer m Int8
 int8 = contramap fromIntegral word8
+
+{- | Serialize a 'Word16' in little-endian format. -}
+word16Le :: Binary m => Serializer m Word16
+word16Le = embed Binary.word16Le
+
+{- | Serialize a 'Word32' in little-endian format. -}
+word32Le :: Binary m => Serializer m Word32
+word32Le = embed Binary.word32Le
+
+{- | Serialize a 'Word64' in little-endian format. -}
+word64Le :: Binary m => Serializer m Word64
+word64Le = embed Binary.word64Le
+
+{- | Serialize a 'Int16' in little-endian format. -}
+int16Le :: Binary m => Serializer m Int16
+int16Le = embed Binary.int16Le
+
+{- | Serialize a 'Int32' in little-endian format. -}
+int32Le :: Binary m => Serializer m Int32
+int32Le = embed Binary.int32Le
+
+{- | Serialize a 'Int64' in little-endian format. -}
+int64Le :: Binary m => Serializer m Int64
+int64Le = embed Binary.int64Le
+
+{- | Serialize a 'Word16' in big-endian format. -}
+word16Be :: Binary m => Serializer m Word16
+word16Be = embed Binary.word16Be
+
+{- | Serialize a 'Word32' in big-endian format. -}
+word32Be :: Binary m => Serializer m Word32
+word32Be = embed Binary.word32Be
+
+{- | Serialize a 'Word64' in big-endian format. -}
+word64Be :: Binary m => Serializer m Word64
+word64Be = embed Binary.word64Be
+
+{- | Serialize a 'Int16' in big-endian format. -}
+int16Be :: Binary m => Serializer m Int16
+int16Be = embed Binary.int16Be
+
+{- | Serialize a 'Int32' in big-endian format. -}
+int32Be :: Binary m => Serializer m Int32
+int32Be = embed Binary.int32Be
+
+{- | Serialize a 'Int64' in big-endian format. -}
+int64Be :: Binary m => Serializer m Int64
+int64Be = embed Binary.int64Be
+
+
+{- | Serializer for lists. -}
+sizedOf :: Binary m => Serializer m Int -> Serializer m a -> Serializer m [a]
+sizedOf len val = contramap length len <> listOf val
+
+
+{- | Serialize a 'Char'.
+
+Truncates 'Char' to 'Word8'. It is the responsability of the caller to sanitize the input.
+-}
+char :: Binary m => Serializer m Char
+char =  contramap (fromIntegral . ord) word8
+
+{- | Serialize a 'String'.
+
+Truncates 'Char' to 'Word8'. It is the responsability of the caller to sanitize the input.
+-}
+string :: Binary m => Serializer m String
+string = listOf char
---
+++ src/Trisagion/Serializers/Combinators.hs
@@ -0,0 +1,19 @@
+{- |
+Module: Trisagion.Serializers.Combinators
+
+Various serializer combinators.
+-}
+
+module Trisagion.Serializers.Combinators (
+    -- * List combinators.
+    listOf,
+) where
+
+-- Imports.
+-- Package.
+import Trisagion.Serializer (Serializer, embed, serialize)
+
+
+{- | Serializer for lists. -}
+listOf :: Monoid m => Serializer m a -> Serializer m [a]
+listOf h = embed $ foldMap (serialize h) 
