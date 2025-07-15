6e8d890c2c2e6a621dcb6010d86a3cc6d1ce6cc3
lambda-dom <35112850+lambda-dom@users.noreply.github.com>
2025-07-15 15:40:45 +0100

    `Builder` serializers. (#69)
    * `Builder` serializers.
    
    * Haddock fixes.
    
    * README.md quote.
    
    * with-compiler field.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Trisagion.
 
+> Abba Moses asked Abba Sylvanus, "Can a man lay a new foundation every day?" The old man said, "If he works hard, he can lay a new foundation at every moment."
+> -- The Sayings of the Desert Fathers.
+
 A monadic parsing and serializing library.
---
+++ src/Trisagion/Serializers/Builder.hs
@@ -0,0 +1,34 @@
+{- |
+Module: Trisagion.Serializers.Builder
+
+Serializers @'Builder' m => 'Serializer' m a@.
+-}
+
+module Trisagion.Serializers.Builder (
+    -- * Serializers @'Builder' m => 'Serializer' m a@.
+    one,
+    many,
+    pack,
+) where
+
+-- Imports.
+-- non-Hackage libraries.
+import Mono.Typeclasses.MonoFunctor (ElementOf)
+
+-- Package.
+import Trisagion.Typeclasses.Builder (Builder (BuilderOf))
+import qualified Trisagion.Typeclasses.Builder as Builder (one, many, pack)
+import Trisagion.Serializer (Serializer, embed)
+
+
+{- | Serialize a single @'ElementOf' ('BuilderOf' m)@. -}
+one :: Builder m => Serializer m (ElementOf (BuilderOf m))
+one = embed Builder.one
+
+{- | Serialize a foldable of @'ElementOf' ('BuilderOf' m)@. -}
+many :: (Builder m, Foldable t) => Serializer m (t (ElementOf (BuilderOf m)))
+many = embed Builder.many
+
+{- | Serialize a @'BuilderOf' m@. -}
+pack :: Builder m => Serializer m (BuilderOf m)
+pack = embed Builder.pack
