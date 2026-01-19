bf570c98205254ce9722d8dbf093d6bd03ee8d09
lambda-dom <grodrigues.math@gmail.com>
2026-01-19 20:17:40 +0000

    `Streamable` in terms of parsers.

--- src/Trisagion/Typeclasses/Streamable.hs
+++ src/Trisagion/Typeclasses/Streamable.hs
@@ -5,64 +5,153 @@ The @Streamable@ typeclass.
 -}
 
 module Trisagion.Typeclasses.Streamable (
+    -- * Error types.
+    InputError (..),
+    ValidationError (..),
+
     -- * Typeclasses.
     Streamable (..),
+
+    -- * Parsers @'Streamable' m a s => 'ParserT' m s e a@.
+    satisfy,
+    single,
+    oneOf,
 ) where
 
 -- Imports.
 -- Base.
-import Data.Maybe (isNothing)
-import Data.List (uncons, singleton)
+import Data.Functor.Identity (Identity (..))
+import Data.Void (Void)
+
+-- Libraries.
+import Control.Monad.State (MonadState (..))
 
 -- Non-hackage libraries.
 import Mono.Typeclasses.MonoFunctor (MonoFunctor (..))
 
+-- Package.
+import Trisagion.ParserT (ParserT, throw, lookAhead, validate)
+import Trisagion.Parsers.Combinators (skip)
+import Trisagion.Types.Either ((:+:))
+
+
+{- | The @InputError@ error type. -}
+newtype InputError = InputError Word
+    deriving stock (Eq, Show)
+
+
+{- | Monofunctoriality of t'InputError'. -}
+instance MonoFunctor Word InputError where
+    {-# INLINE monomap #-}
+    monomap :: (Word -> Word) -> InputError -> InputError
+    monomap f (InputError n) = InputError (f n)
+
+
+{- | The @ValidationError@ error tag type thrown on failed validations. -}
+newtype ValidationError e = ValidationError e
+    deriving stock (Eq, Show, Functor, Foldable, Traversable)
+    deriving (Applicative, Monad) via Identity
+
 
 {- | The @Streamable@ typeclass of input streams. -}
 class (Monad m, MonoFunctor a s) => Streamable m a s | s -> a where
-    {-# MINIMAL unconsM #-}
+    {-# MINIMAL one #-}
 
-    {- | Get the first element from the stream. -}
-    unconsM :: s -> m (Maybe (a, s))
+    {- | Parser getting the first element from the stream. -}
+    one :: ParserT s InputError m a
 
-    {- | Convert a stream to a list. -}
-    toListM :: s -> m [a]
-    toListM xs = do
-        x <- unconsM xs
-        case x of
-            Nothing      -> pure []
-            Just (y, ys) -> (y :) <$> toListM ys
+    {- | Skip one element from the input stream. -}
+    skipOne :: ParserT s InputError m ()
+    skipOne = skip one
 
-    {- | Monadic check for the end of the input stream. -}
-    nullM :: s -> m Bool
-    nullM = fmap (maybe True (const False)) . unconsM
+    {- | Parse one element from the input stream but without consuming input. -}
+    peek :: ParserT s Void m (Maybe a)
+    peek = either (const Nothing) Just <$> lookAhead one
+
+    {- | Parser returning true if the input stream is exhausted. -}
+    eoi :: ParserT s Void m Bool
+    eoi = either (const True) (const False) <$> lookAhead one
 
 
 -- Instances.
 instance Monad m => Streamable m a (Maybe a) where
-    {-# INLINE unconsM #-}
-    unconsM :: Maybe a -> m (Maybe (a, Maybe a))
-    unconsM xs = case xs of
-        Nothing -> pure $ Nothing
-        Just x  -> pure $ Just (x, Nothing)
+    {-# INLINE one #-}
+    one :: ParserT (Maybe a) InputError m a
+    one = do
+        xs <- get
+        case xs of
+            Just x -> put Nothing *> pure x
+            _      -> throw $ InputError 1
 
-    {-# INLINE toListM #-}
-    toListM :: Maybe a -> m [a]
-    toListM = pure . maybe [] singleton
+    {-# INLINE skipOne #-}
+    skipOne :: ParserT (Maybe a) InputError m ()
+    skipOne = do
+        xs <- get
+        case xs of
+            Just _ -> put Nothing *> pure ()
+            _      -> throw $ InputError 1
 
-    {-# INLINE nullM #-}
-    nullM :: Maybe a -> m Bool
-    nullM = pure . isNothing
+    {-# INLINE peek #-}
+    peek :: ParserT (Maybe a) Void m (Maybe a)
+    peek = get
+
+    {-# INLINE eoi #-}
+    eoi :: ParserT (Maybe a) Void m Bool
+    eoi = get >>= maybe (pure True) (const $ pure False)
 
 instance Monad m => Streamable m a [a] where
-    {-# INLINE unconsM #-}
-    unconsM :: [a] -> m (Maybe (a, [a]))
-    unconsM = pure . uncons
+    {-# INLINE one #-}
+    one :: ParserT [a] InputError m a
+    one = do
+        xs <- get
+        case xs of
+            (y : ys) -> put ys *> pure y
+            _        -> throw $ InputError 1
 
-    {-# INLINE toListM #-}
-    toListM :: [a] -> m [a]
-    toListM = pure
+    {-# INLINE skipOne #-}
+    skipOne :: ParserT [a] InputError m ()
+    skipOne = do
+        xs <- get
+        case xs of
+            (_ : ys) -> put ys *> pure ()
+            _        -> throw $ InputError 1
 
-    {-# INLINE nullM #-}
-    nullM :: [a] -> m Bool
-    nullM = pure . null
+    {-# INLINE peek #-}
+    peek :: ParserT [a] Void m (Maybe a)
+    peek = get >>= pure . headMaybe
+        where
+            headMaybe :: [a] -> Maybe a
+            headMaybe (x : _) = Just x
+            headMaybe _       = Nothing
+
+    {-# INLINE eoi #-}
+    eoi :: ParserT [a] Void m Bool
+    eoi = get >>= pure . null
+
+
+{- | Parse one element from the input stream satisfying a predicate. -}
+{-# INLINE satisfy #-}
+satisfy
+    :: forall m a s . Streamable m a s
+    => (a -> Bool)                      -- ^ Predicate on @a@.
+    -> ParserT s (ValidationError a :+: InputError) m a
+satisfy p = validate v one
+    where
+        v :: a -> ValidationError a :+: a
+        v x = if p x then Left (ValidationError x) else Right x
+
+{- | Parse one element from the input stream matching an @x :: a@. -}
+{-# INLINE single #-}
+single
+    :: (Streamable m a s, Eq a)
+    => a                                -- ^ Matching @x :: a@.
+    -> ParserT s (ValidationError a :+: InputError) m a
+single x = satisfy (== x)
+
+{- | Parse one element from the input stream that is an element of a foldable. -}
+{-# INLINE oneOf #-}
+oneOf
+    :: (Streamable m a s, Eq a, Foldable t)
+    => t a                              -- ^ Foldable of @a@'s against which to test inclusion.
+    -> ParserT s (ValidationError a :+: InputError) m a
+oneOf xs = satisfy (`elem` xs)
