9ef08cfe8f70a5471e0e46e0deb04e7bcaaeb28f
lambda-dom <grodrigues.math@gmail.com>
2026-01-20 11:34:55 +0000

    The `Binary` typeclass.

--- src/Trisagion/Typeclasses/Binary.hs
+++ src/Trisagion/Typeclasses/Binary.hs
@@ -5,6 +5,9 @@ The @Binary@ for parsers with constraints @'Streamable' m Word8 s@.
 -}
 
 module Trisagion.Typeclasses.Binary (
+    -- * Typeclasses.
+    Binary (..),
+
     -- * Generic parsers.
     integralLe,
     integralBe,
@@ -13,16 +16,52 @@ module Trisagion.Typeclasses.Binary (
 -- Imports.
 -- Base.
 import Data.Bits (FiniteBits)
-import Data.Word (Word8)
+import Data.Int (Int8)
+import Data.Word (Word8, Word16, Word32, Word64)
 
 -- non-Hackage libraries.
 import Mono.Typeclasses.MonoFoldable (MonoFoldable (..))
 
 -- Package.
-import Trisagion.Utils.Integral (byteCount, pack, packReverse)
+import Trisagion.Utils.Bits (byteCount, pack, packReverse)
 import Trisagion.ParserT (ParserT)
-import Trisagion.Typeclasses.Streamable (InputError)
+import Trisagion.Typeclasses.Streamable (InputError, Streamable (..))
 import Trisagion.Typeclasses.Splittable (Splittable (..))
+
+
+{- | The @Binary@ typeclass for efficient parsers for machine-width types. -}
+class (Splittable m Word8 b s, MonoFoldable Word8 b) => Binary m b s where
+    {- | Parse a single 'Word8'. -}
+    word8 :: ParserT s InputError m Word8
+    word8 = one
+
+    {- | Parse a single 'Int8'. -}
+    int8 :: ParserT s InputError m Int8
+    int8 = fromIntegral <$> word8
+
+    {- | Parse a 'Word16' in little-endian format. -}
+    word16Le :: ParserT s InputError m Word16
+    word16Le = integralLe
+
+    {- | Parse a 'Word32' in little-endian format. -}
+    word32Le :: ParserT s InputError m Word32
+    word32Le = integralLe
+
+    {- | Parse a 'Word64' in little-endian format. -}
+    word64Le :: ParserT s InputError m Word64
+    word64Le = integralLe
+
+    {- | Parse a 'Word16' in big-endian format. -}
+    word16Be :: ParserT s InputError m Word16
+    word16Be = integralBe
+
+    {- | Parse a 'Word32' in big-endian format. -}
+    word32Be :: ParserT s InputError m Word32
+    word32Be = integralBe
+
+    {- | Parse a 'Word64' in big-endian format. -}
+    word64Be :: ParserT s InputError m Word64
+    word64Be = integralBe
 
 
 {- | Parse a machine-width integral in little-endian format. -}
