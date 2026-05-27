a325e7f4998393d0b58a2c587f1d7266cd4f77e5
lambda-dom <grodrigues.math@gmail.com>
2026-05-27 18:16:58 +0100

    The `Parser` monad.

--- src/Trisagion/Parser.hs
+++ src/Trisagion/Parser.hs
@@ -6,4 +6,56 @@ The @Parser@ monad transformer.
 
 module Trisagion.Parser (
     -- * The parsing monad.
+    Parser,
+
+    -- * Basic functions.
+    embed,
+    run,
+    parse,
+    eval,
+    remainder,
 ) where
+
+-- Imports.
+-- Base.
+import Data.Kind (Type)
+
+-- Package.
+import Trisagion.Utils.Either ((:+:))
+import Trisagion.Types.Result (Result (..), toEither)
+
+
+{- | The parsing monad @Parser s e a@.
+
+@s@ is the input stream type, @e@ is the type of parsing errors that the parser can throw and @a@
+is the type of parsed values.
+-}
+type Parser ::  Type -> Type -> Type -> Type
+newtype Parser s e a = Parser (s -> Result s e a)
+    deriving stock Functor
+
+
+{- | Embed a parsing function in the t'Parser' monad. -}
+{-# INLINE embed #-}
+embed ::  (s ->  Result s e a) -> Parser s e a
+embed = Parser
+
+{- | Run the parser on the input and return the results. -}
+{-# INLINE run #-}
+run :: Parser s e a -> s ->  Result s e a
+run (Parser f) = f
+
+{- | Parse the input and return the result as an 'Either'. -}
+{-# INLINE parse #-}
+parse :: Parser s e a -> s -> e :+: (a, s)
+parse p = toEither . run p
+
+{- | Evaluate the parser on the input and return the result, discarding the remainder. -}
+{-# INLINE eval #-}
+eval :: Parser s e a -> s -> e :+: a
+eval p = fmap fst . parse p
+
+{- | Run the parser on the input and return the remainder, discarding the parsed value. -}
+{-# INLINE remainder #-}
+remainder :: Parser s e a -> s -> e :+: s
+remainder p = fmap snd . parse p
