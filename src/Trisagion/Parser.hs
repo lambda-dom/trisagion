d0a6b793530695062dc7fdd43cd28b3562b0ac43
lambda-dom <grodrigues.math@gmail.com>
2026-05-27 17:29:20 +0100

    The `Result` type.

---
+++ src/Trisagion/Parser.hs
@@ -0,0 +1,9 @@
+{- |
+Module: Trisagion.Parser
+
+The @Parser@ monad transformer.
+-}
+
+module Trisagion.Parser (
+    -- * The parsing monad.
+) where
---
+++ src/Trisagion/Types/Result.hs
@@ -0,0 +1,44 @@
+{- |
+Module: Trisagion.Types.Result
+
+The return type of parsing functions.
+-}
+
+module Trisagion.Types.Result (
+    -- * Types.
+    Result (..),
+
+    -- ** Isomorphisms.
+    toEither,
+) where
+
+-- Imports.
+-- Base.
+import Data.Bifunctor (Bifunctor (..))
+import Data.Kind (Type)
+
+-- Package.
+import Trisagion.Utils.Either ((:+:))
+
+
+{- | The 'Result' type of a parsing function, isomorphic to @'Either' e (a, s)@. -}
+type Result :: Type -> Type -> Type -> Type
+data Result s e a
+    = Error e                           -- ^ Error case.
+    | Success a !s                      -- ^ Success case.
+    deriving stock (Eq, Show, Functor)
+
+
+-- Instances.
+instance Bifunctor (Result s) where
+    {-# INLINE bimap #-}
+    bimap :: (d -> e) -> (a -> b) -> Result s d a -> Result s e b
+    bimap f _ (Error e)      = Error (f e)
+    bimap _ g (Success x xs) = Success (g x) xs
+
+
+{- | The isomorphism @'Result' s e a -> 'Either' e (a, s)@. -}
+{-# INLINE toEither #-}
+toEither :: Result s e a -> e :+: (a, s)
+toEither (Error e)      = Left e
+toEither (Success x xs) = Right (x, xs)
