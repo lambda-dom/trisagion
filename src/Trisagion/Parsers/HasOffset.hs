2313725c19ecabc7fe612115dac3a865a6b82899
lambda-dom <35112850+lambda-dom@users.noreply.github.com>
2026-05-28 12:04:33 +0100

    `HasOffset` typeclass and parsers. (#6)

---
+++ src/Trisagion/Parsers/HasOffset.hs
@@ -0,0 +1,28 @@
+{- |
+Module: Trisagion.Parsers.Streamable
+
+Parsers with @'HasOffset' s@ constraints.
+-}
+
+module Trisagion.Parsers.HasOffset (
+    -- * Parsers @'HasOffset' s => 'Parser' s e a@.
+    offset,
+) where
+
+-- Imports.
+-- Base.
+import Data.Void (Void)
+
+-- Libraries.
+import Control.Monad.State (gets)
+
+-- Package.
+import Trisagion.Typeclasses.HasOffset (HasOffset)
+import qualified Trisagion.Typeclasses.HasOffset as HasOffset (offset)
+import Trisagion.Parser (Parser)
+
+
+{- | Parser returning the current stream offset. -}
+{-# INLINE offset #-}
+offset :: HasOffset s => Parser s Void Word
+offset = gets HasOffset.offset
---
+++ src/Trisagion/Typeclasses/HasOffset.hs
@@ -0,0 +1,22 @@
+{- |
+Module: Trisagion.Typeclasses.HasOffset
+
+The @HasOffset@ typeclass.
+-}
+
+module Trisagion.Typeclasses.HasOffset (
+    -- * Typeclasses.
+    HasOffset (..),
+) where
+
+-- Imports.
+-- Base.
+import Data.Kind (Type)
+
+
+{- | The typeclass for input streams with a notion of current offset. -}
+class HasOffset (s :: Type) where
+    {-# MINIMAL offset #-}
+
+    {- | Monadic getter fetching the current stream offset. -}
+    offset :: s -> Word
