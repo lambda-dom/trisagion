730b12173143c98861f82dac4e1fea3ad7c35a8d
lambda-dom <grodrigues.math@gmail.com>
2026-05-28 12:15:18 +0100

    `ParseError` type and parsers.

---
+++ src/Trisagion/Types/ParseError.hs
@@ -0,0 +1,45 @@
+{- |
+Module: Trisagion.Types.ParseError
+
+The @ParseError@ error type.
+-}
+
+module Trisagion.Types.ParseError (
+    -- * Types.
+    ParseError (..),
+) where
+
+-- Imports.
+-- Base.
+import Data.Kind (Type)
+
+
+{- | The @ParseError@ type with error tag @e@. -}
+type ParseError :: Type -> Type
+data ParseError e
+    = Failure
+    | ParseError !Word e
+    deriving stock (Eq, Show, Functor)
+
+
+{- | The 'Semigroup' instance of t'ParseError'.
+
+The 'Semigroup' instance implements short-circuiting by returning the left, or first, non-identity
+element. The instance is idempotent and for every @f :: d -> e@,
+
+@
+'fmap' f :: t'ParseError' d -> t'ParseError' e
+@
+
+is a monoid morphism.
+-}
+instance Semigroup (ParseError e) where
+    {-# INLINE (<>) #-}
+    (<>) :: ParseError e -> ParseError e -> ParseError e
+    (<>) Failure e = e
+    (<>) e       _ = e
+
+instance Monoid (ParseError e) where
+    {-# INLINE mempty #-}
+    mempty :: ParseError e
+    mempty = Failure
