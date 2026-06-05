12fb06bd546aad16357a46087dccb6873f75e2c7
lambda-dom <35112850+lambda-dom@users.noreply.github.com>
2026-06-05 11:37:44 +0100

    Examples for functions in Utils.List. (#20)

--- src/Trisagion/Utils/List.hs
+++ src/Trisagion/Utils/List.hs
@@ -30,16 +30,37 @@ enumDown n = if n < 0 then const [] else zip ns
     where
         ns = if n == 0 then [0] else [n, pred n .. 0]
 
-{- | Exact version of 'splitAt'. -}
+{- | Exact version of 'splitAt'.
+
+=== __Examples:__
+
+>>> splitAtExact 3 "01234"
+Just ("012","34")
+
+>>> splitAtExact 0 "01234"
+Just ("","01234")
+
+>>> splitAtExact 10 "01234"
+Nothing
+-}
 {-# INLINEABLE splitAtExact #-}
 splitAtExact :: Int -> [a] -> Maybe ([a], [a])
 splitAtExact n xs
-    | n <= 0 = Just ([], xs)
+    | n <= 0    = Just ([], xs)
     | otherwise = case xs of
         []       -> Nothing
         (y : ys) -> fmap (first (y :)) $ splitAtExact (pred n) ys
 
-{- | Match a prefix list, returning the tail. -}
+{- | Match a prefix list, returning the tail.
+
+=== __Examples:__
+
+>>> matchPrefix "012" "01234"
+Just "34"
+
+>>> matchPrefix "012" "01"
+Nothing
+-}
 {-# INLINEABLE matchPrefix #-}
 matchPrefix :: Eq b => [b] -> [b] -> Maybe [b]
 matchPrefix []       x        = Just x
